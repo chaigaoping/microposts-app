3bf50c1e2b62b8a6cdd191dc3c6df54351a4309f
apple <chgpgf@gamil.com>
2017-06-07 10:10:02 +0800

    implement 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
 
@@ -45,6 +46,20 @@ class UsersController < ApplicationController
     redirect_to user_url
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
   private
 
@@ -53,7 +68,7 @@ class UsersController < ApplicationController
                                    :password_confirmation)
   end
 
-  
+
 
   def correct_user
     @user = User.find(params[:id])
@@ -63,4 +78,6 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
+
+
 end
